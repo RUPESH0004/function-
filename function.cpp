d6369a8be9612916633aa3a76387a4010c743a51
RUPESH0004 <rj9082356865@gmail.com>
2023-12-01 18:15:00 +0530

    practice for using function

--- function.cpp
+++ function.cpp
@@ -1,17 +1,62 @@
 #include <iostream>
 using namespace std;
+// This function for fibonacchi serise
 int fib(int n)
 {
     int a = 0, b = 1;
+    cout<<"the fibonacchi series is:-";
     cout << a << " ";
     cout << b << " ";
     for (int i = 3; i <= n; i++)
     {
         int ans = a + b;
-        cout << ans <<" ";
+        cout << ans << " ";
         a = b;
         b = ans;
     }
+    cout << endl;
+}
+//This finction is for prinme no
+void isPrime(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "The given no is Prime" << endl;
+            break;
+        }
+        else
+        {
+            cout << "The given no is not prime no " << endl;
+            break;
+        }
+        cout << endl;
+    }
+}
+//This finction is fot counting no 
+int PrintCounting(int n)
+{
+    cout <<"the counting is:- ";
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+//This finction is for factorial
+int factorial(int n)
+{
+    cout <<"the factorial is :-";
+    int fact = n;
+    for (int i = 1; i < n; i++)
+    {
+
+        fact = fact * i;
+    }
+
+    cout << fact << " ";
+    cout << endl;
 }
 int main()
 {
@@ -19,4 +64,7 @@ int main()
     cout << "Enter the no you want fibonacchi" << endl;
     cin >> n;
     fib(n);
+    isPrime(n);
+    PrintCounting(n);
+    factorial(n);
 }
